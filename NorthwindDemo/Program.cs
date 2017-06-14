80dc987a35e02815c2543e6ceba8c015c36169a1
enriqueescobar-askida <Enrique.Escobar@genome.ulaval.ca>
2017-06-14 14:22:02 -0400

    baseline console baseline with app.config change to match sql server and not localDB

---
+++ NorthwindDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace NorthwindDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new TileboardLocatorContext())
+            {
+                /*var data = db.Categories;
+                foreach (var item in data)
+                {
+                    Console.WriteLine(item.CategoryId + " " + item.CategoryName);
+                }
+                Console.WriteLine();
+
+                var history = db.CustOrderHist("ALFKI");
+                foreach (var item in history)
+                {
+                    Console.WriteLine("{0, 2} {1}", item.Total, item.ProductName);
+                }
+
+                Console.WriteLine();
+
+                var orderDetail = db.OrderDetails.First();
+                Console.WriteLine(orderDetail.Product.ProductName);*/
+            }
+        }
+    }
+}
